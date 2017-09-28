b6e3774029ffee9357c188df31d89fff12cf137e
Patrick Zvonarits <Patrick Zvonarits>
2017-09-28 13:15:17 +0200

    Add project files.

---
+++ Ak-Tool_280917/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ak_Tool_280917
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
